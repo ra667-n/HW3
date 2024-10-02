21314160e954be7f8e4369ff74eef63261078bd5
Roheemat Adebiyi <ra667@njit.edu>
2024-10-02 00:01:54 -0400

    Update __init__.py

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -3,3 +3,23 @@
 
 def subtract(a,b):
     return a - b
+ 
+def multiply(a,b):
+    return a * b
+
+def divide(a,b)
+    if b == 0:
+        raise DivisionByZero("Cannot divide by zero")
+    return a/b
+
+class MyCalculation:
+   def __init__(self, operation, result):
+        self.operation = operation
+        self.result = result
+
+class My Calculations:
+    history = []
+
+
+        def calculation_lg(self, calc: MyCalculation):
+            self.history.append(calc)
